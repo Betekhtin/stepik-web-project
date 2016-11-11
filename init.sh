3065f391d061250a03defe8788b69bab0f5378e3
Betekhtin <st040186@student.spbu.ru>
2016-11-12 02:33:06 +0500

    task 1.9 finally works!

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-#gunicorn -b 0.0.0.0:8080 hello:app
+gunicorn -b 0.0.0.0:8080 hello:application
