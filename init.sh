b5caaa05325a5c449d9ef0f5ae82982b0969e7e2
Betekhtin <st040186@student.spbu.ru>
2016-11-14 00:11:48 +0300

    task 2.1 init update

--- init.sh
+++ init.sh
@@ -2,4 +2,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-gunicorn -b 0.0.0.0:8080 hello:application
+gunicorn -b 0.0.0.0:8080 --pythinpath /home/box/web hello:application
+gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask ask.wsgi:application
